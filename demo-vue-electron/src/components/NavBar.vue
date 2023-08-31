4eb1e49805b5b44a954b30d8bee2a242d831d1b5
Yannick Charron <ycharron@cstj.qc.ca>
2023-08-31 15:16:23 -0400

    Fin du cours 31 août

--- demo-vue-electron/src/components/NavBar.vue
+++ demo-vue-electron/src/components/NavBar.vue
@@ -8,7 +8,12 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
           <ul class="navbar-nav me-auto mb-2 mb-md-0">
-            
+            <li class="nav-item">
+              <router-link class="nav-link" :to="{name:'home'}">Accueil</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" :to="{name:'planets'}">Planets</router-link>
+            </li>
           </ul>
         </div>
       </div>
--- demo-vue-electron/src/main.ts
+++ demo-vue-electron/src/main.ts
@@ -4,9 +4,13 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 
+import axios from 'axios'
+
 const app = createApp(App)
 
 app.use(router)
+
+app.provide('axios', axios)
 
 app.mount('#app')
   .$nextTick(() => postMessage({ payload: 'removeLoading' }, '*'))
