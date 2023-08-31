f4c3a62c333ccde727b24afedda9c3e46e61383b
Yannick Charron <ycharron@cstj.qc.ca>
2023-08-31 13:35:19 -0400

    Cours du 29 août

---
+++ demo-vue-electron/src/views/pages/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+    <DefaultLayout>
+        <p>Page d'accueil</p>
+        <p>ysadbfhdsaviuophvuiervb3grgfuoerugheufo8wfuoireq</p>
+    </DefaultLayout>    
+</template>
+
+<script setup lang="ts">
+import DefaultLayout from '../layouts/DefaultLayout.vue'
+</script>
+
+<style scoped>
+
+</style>
