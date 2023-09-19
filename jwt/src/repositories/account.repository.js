09ccb38a75f60d6831356f455c589e4f2ec20f89
Yannick Charron <ycharron@cstj.qc.ca>
2023-09-19 17:58:43 -0400

    feature: Login et Login route

--- jwt/src/repositories/account.repository.js
+++ jwt/src/repositories/account.repository.js
@@ -1,11 +1,46 @@
+import HttpError from 'http-errors';
+import { v4 as uuidv4 } from 'uuid';
+import argon2d from 'argon2';
 
 import { Account } from '../models/account.model.js';
 
 class AccountRepository {
 
-    create(account) {
+    async login(email, username, password) {
+
+        const account = await Account.findOne({$or: [{email: email}, {username: username}]});
+        if(!account) {
+            return { err: HttpError.Unauthorized() }
+        }
+        //Nous avons un compte avec le email ou username
+        //Vérification du bon mot de passe
+        if(await this.validatePassword(password, account)) {
+            return { account };
+        } else {
+            // Mauvais mot de passe
+            return { err: HttpError.Unauthorized() }
+        }
+
+    }
+
+    generateJWT() {
+        
+    }
+
+
+    async validatePassword(password, account) {
         try {
-            //TODO: Hash password
+            return await argon2d.verify(account.passwordHash, password);
+        } catch(err) {
+            throw err;
+        }
+    }
+
+    async create(account) {
+        try {
+            account.uuid = uuidv4();
+            account.passwordHash = await argon2d.hash(account.password);
+            delete account.password; //supprime le mot de passe en clair de l'objet avant de le sauvegarder
             return Account.create(account);
         } catch(err) {
             throw err;
--- jwt/src/routes/accounts.routes.js
+++ jwt/src/routes/accounts.routes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import HttpError from 'http-errors';
 import accountRepository from '../repositories/account.repository.js';
 
 const router = express.Router();
@@ -7,7 +8,26 @@ class AccountRoutes {
 
     constructor() {
         router.post('/', this.post);
+        router.post('/actions/login', this.login);
 
+    }
+
+    async login(req, res, next) {
+        const { email, username, password } = req.body;
+        if((email && username) || email === "" || username === "") {
+            return next(HttpError.BadRequest(''));
+        }
+
+        const result = await accountRepository.login(email, username, password);
+        if(result.account) {
+            //Nous sommes connectés
+            let account = result.account.toObject({getters:false, virtuals:false});
+            //TODO: Genération des JWTs
+            res.status(201).json(account);
+        } else {
+            //Erreur lors de la connexion
+            return next(result.err);
+        }
     }
 
     async post(req, res, next) {
